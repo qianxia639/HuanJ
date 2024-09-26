ab7577ec473ba5a7842af37d278dcd9fbde1aeb3
qianxia639 <2274000859@qq.com>
2024-09-26 17:16:22 +0800

    update user model

--- db/model/user.go
+++ db/model/user.go
@@ -3,28 +3,17 @@ package model
 import "time"
 
 type User struct {
-	// 用户Id
-	ID int32 `json:"id,omitempty"`
-	// 用户名
+	ID       uint32 `json:"id,omitempty"`
 	Username string `json:"username,omitempty"`
-	// 用户昵称
 	Nickname string `json:"nickname,omitempty"`
-	// 密码
 	Password string `json:"-"`
-	// 邮箱
-	Email string `json:"email,omitempty"`
+	Salt     string `json:"-"`
+	Email    string `json:"email,omitempty"`
 	// 性别, 1 男, 2 女, 3 未知
-	Gender int8 `json:"gender,omitempty"`
-	// 头像图片路径或链接
-	ProfilePictureUrl string `json:"profile_picture_url,omitempty"`
-	// 在线状态(在线/离线)
-	OnlineStatus bool `json:"online_status"`
-	// 密码更新时间
+	Gender            int8      `json:"gender,omitempty"`
+	ProfilePictureUrl string    `json:"profile_picture_url,omitempty"`
+	IsOnline          bool      `json:"online_status"`
 	PasswordChangedAt time.Time `json:"password_changed_at,omitempty"`
-	// 最后在线时间
-	LastLoginAt time.Time `json:"last_login_at,omitempty"`
-	// 创建时间
-	CreatedAt time.Time `json:"created_at,omitempty"`
-	// 更新时间
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	CreatedAt         time.Time `json:"created_at,omitempty"`
+	UpdatedAt         time.Time `json:"updated_at,omitempty"`
 }
