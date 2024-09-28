3d754a7d9128f9ae8174e96501d2d36dd6e365e3
qianxia639 <2274000859@qq.com>
2024-09-28 17:24:03 +0800

    created user service

--- handler/user_service.go
+++ handler/user_service.go
@@ -7,6 +7,41 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type CreateUserRequest struct {
+	Username      string `json:"username" binding:"required"`
+	Password      string `json:"password" binding:"required"`
+	CheckPassword string `json:"check_password" binding:"required"`
+	Nickname      string `json:"nickname" binding:"required"`
+	Email         string `json:"email" binding:"required"`
+	Gender        int8   `json:"gender"`
+}
+
+func CreateUser(ctx *gin.Context) {
+	var req CreateUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "参数错误",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	// 判断密码是否一致
+	if req.Password != req.CheckPassword {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "密码不一致"})
+		return
+	}
+
+	// 判断性别是否合法
+
+	// 判断用户是否存在
+
+	// 密码加密
+
+	// 创建用户
+
+}
+
 type LoginRequest struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password"`
