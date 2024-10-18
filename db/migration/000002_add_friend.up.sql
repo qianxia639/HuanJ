e4a45a6c8370f7d39f5d81cf3b0951f8310fa752
qianxia639 <2274000859@qq.com>
2024-10-18 15:25:31 +0800

    feat: created friend sql file

---
+++ db/migration/000002_add_friend.up.sql
@@ -0,0 +1,22 @@
+-- 好友表
+CREATE TABLE IF NOT EXISTS "friends" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT NOT NULL,
+    "friend_id" INT NOT NULL,
+    "status" SMALLINT NOT NULL DEFAULT 1,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(user_id,friend_id),  -- 确保一对好友关系唯一
+)
+
+COMMENT ON COLUMN "friends"."id" IS '好友关系标识';
+
+COMMENT ON COLUMN "friends"."user_id" IS '用户ID';
+
+COMMENT ON COLUMN "friends"."friend_id" IS '好友的用户ID';
+
+COMMENT ON COLUMN "friends"."status" IS '关系状态, 1: 待确认, 2: 已确认, 3: 已拒绝';
+
+COMMENT ON COLUMN "friends"."created_at" IS '关系创建时间';
+
+COMMENT ON COLUMN "friends"."created_at" IS '更新时间';
